4721bc318cfaf1b014fcfd5c0a29db10dee37606
Barry Weeks <bweeks101@gmail.com>
2021-02-12 19:45:48 +0000

    Login/Register Template and basic styling per CI task-manager mini-project.  Added role_id and email fields to registration.  script.py linted with eslint.

--- app.py
+++ app.py
@@ -19,8 +19,73 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-def test():
-    return "Is this working?"
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"user_id": request.form.get("user_id").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "role_id": ObjectId("6026ba89f53ae31761a5a24a"),
+            "user_id": request.form.get("user_id").lower(),
+            "pwd": generate_password_hash(request.form.get("pwd")),
+            "email": request.form.get("email").lower()
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("user_id").lower()
+        flash("Registration Successful")
+        return redirect(url_for("home", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"user_id": request.form.get("user_id").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["pwd"], request.form.get("pwd")):
+                session["user"] = request.form.get("user_id").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("user_id")))
+                return redirect(url_for(
+                    "home", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
