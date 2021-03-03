4236cba1d19960cda4cb2015cee14b2d95b4dfdd
Barry Weeks <bweeks101@gmail.com>
2021-03-03 13:37:15 +0000

    Relocated createRoleArray(), getRole(), createUserArray(), getUser(), inputHelperLabel() from editusers.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+/* global roleList, userList */
+
 /* ============================================ */
 /* Password Confirmation Code Modified From */
 /* https://jsfiddle.net/SirusDoma/ayf832td/ */
@@ -39,6 +41,74 @@ function pWordValidation(pWordInput, pWordConfInput) {
     $(pWordConfInput).removeClass("invalid").
     addClass("valid");
     return true;
+}
+
+// eslint-disable-next-line no-unused-vars
+function createRoleArray(pyList) {
+    let roleArr = [];
+    pyList.forEach(function (obj) {
+        roleArr.push({"role": obj.role,
+            "member_count": obj.member_count,
+            "role_icon": obj.role_icon.class});
+    });
+    return roleArr;
+}
+
+// eslint-disable-next-line no-unused-vars
+function getRole(roleId) {
+    let result = false;
+    roleList.forEach(function (role) {
+        if (role.role === roleId) {
+            result = {"role": role.role,
+                "member_count": role.member_count,
+                "role_icon": role.role_icon};
+            return true; //Stop iterating when we find the specified role
+        }
+    });
+    return result;
+}
+
+// eslint-disable-next-line no-unused-vars
+function createUserArray(pyList) {
+    let usrArr = [];
+    pyList.forEach(function (obj) {
+        usrArr.push({"user_id": obj.user_id,
+            "email": obj.email,
+            "locked": obj.locked,
+            "group": obj.role_group,
+            "role": obj.role});
+    });
+    return usrArr;
+}
+
+// eslint-disable-next-line no-unused-vars
+function getUser(userId) {
+    let result = false;
+    userList.forEach(function (user) {
+        if (user.user_id === userId) {
+            result = {"user_id": user.user_id,
+                "email": user.email,
+                "locked": user.locked,
+                "group": user.group,
+                "role": user.role};
+            return true; //Stop iterating when we find the specified user
+        }
+    });
+    return result;
+}
+
+// eslint-disable-next-line no-unused-vars
+function inputHelperLabel (elem) {
+    elem = "#" + elem;
+    let label = `${elem} ~ label`;
+    let labelText = $(label).data("default");
+    let labelColor = '';
+    if ($(elem)[0].classList.contains('invalid')) {
+        labelText = $(label).data("error");
+        labelColor = "#F44336";
+    }
+    $(label).html(labelText).
+        css("color", labelColor);
 }
 
 //document ready
