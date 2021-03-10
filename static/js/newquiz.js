8cb7468cb021f3d26ed7ec081c93bb265aadb8d9
Barry Weeks <bweeks101@gmail.com>
2021-03-10 21:39:16 +0000

    newquiz template and style updates. Added multiple choice question functionality.

--- static/js/newquiz.js
+++ static/js/newquiz.js
@@ -1,3 +1,4 @@
+/*eslint func-style: ["error", "declaration", { "allowArrowFunctions": true }]*/
 /* global getCategory */
 
 function getCategoryIconClass(category) {
@@ -64,7 +65,152 @@ function reinitSelectOnDisabled(elem) {
     observer.observe(elem[0], {attributeFilter: ['disabled']});
 }
 
+// eslint-disable-next-line no-unused-vars
+function addMulti(elem) {
+    let answersContainer = $(elem).closest('.answers-container');
+    let prefix = $(elem).closest('.prefix');
+    let qId = parseInt(prefix.attr('data-question'));
+    let mlt = parseInt(prefix.attr('data-multi')) + 1;
+
+    prefix.html("");
+    let controlHtml = `
+        <a href="#!" onclick="removeMulti(this)">-</a>` +
+        `<a href="#!" onclick="addMulti(this)">+</a>`;
+    let answerHtml = `
+        <div class="input-field col s10">
+            <span class="prefix light-blue-text text-darken-4 ` +
+                `center-align" data-question="${qId}"` +
+                `data-multi="${mlt}">
+                ${controlHtml}` +
+            `</span>
+            <input id="answer_${qId}-${mlt}" ` +
+                `name="answer_${qId}-${mlt}" ` +
+                `type="text" minlength="1" maxlength="100" ` +
+                `class="validate" value="" required>
+            <label for="answer_${qId}-${mlt}" ` +
+                `data-error="Invalid Answer" ` +
+                `data-default="Answer ${qId}-${mlt}">` +
+                `Answer ${qId}-${mlt}</label>
+        </div>
+        <div class="input-field col s2 checkbox-container ` +
+            `inline-input center-align">
+            <label class="center-align full-width">
+                <input id="correct_${qId}-${mlt}" ` +
+                    `name="correct_${qId}-${mlt}" ` +
+                    `type="checkbox"/>
+                <span></span>
+            </label>
+        </div>`;
+    answersContainer.append(answerHtml);
+}
+
+// eslint-disable-next-line no-unused-vars
+function removeMulti(elem) {
+    let prefix = $(elem).closest('.prefix');
+    let mlt = parseInt(prefix.attr('data-multi')) - 1;
+    let inputField = prefix.closest('.input-field');
+    let checkboxContainer = inputField.next();
+    let answersContainer = inputField.closest('.answers-container');
+    let prevMulti = $(` .prefix[data-multi="${mlt}"]`, answersContainer);
+
+    let controlHtmlRemove = `<a href="#!" onclick="removeMulti(this)">-</a>`;
+    let controlHtmlAdd = `<a href="#!" onclick="addMulti(this)">+</a>`;
+    let controlHtml;
+    if (mlt === 2) {
+        controlHtml = controlHtmlAdd;
+    } else if (mlt > 2) {
+        controlHtml = controlHtmlRemove + controlHtmlAdd;
+    }
+    prevMulti.html(controlHtml);
+
+    inputField.remove();
+    checkboxContainer.remove();
+}
+
+function checkBoxMulti(elem) {
+    let inputField = $(elem).closest('.checkbox-container');
+    let parentRow = inputField.closest('.row');
+    let questionTitle = $('.question-title', parentRow);
+    let qId = parseInt(questionTitle.attr('data-question'));
+    let targetTitle = inputField.next();
+    let targetField = targetTitle.next();
+    let htmlTitle = `<span class="col s12">What is the Answer?</span>`;
+    let htmlContent = `
+        <div class="input-field col s12">
+            <span class="prefix" data-question="${qId}" data-multi="0"></span>
+            <input id="answer_${qId}" name="answer_${qId}" ` +
+                `type="text" minlength="1" maxlength="100" class="validate" ` +
+                `value="" required>
+            <label for="answer_${qId}" data-error="Invalid Answer" ` +
+                `data-default="Answer ${qId}">Answer ${qId}` +
+                `</label>
+        </div>`;
+    let multiCount = 0;
+    if ($(elem).prop('checked')) {
+        multiCount = 1;
+        htmlTitle = `<span class="col s10">What are the Choices?</span>` +
+            `<span class="col s2 center-align">Correct?</span>`;
+        htmlContent = "";
+        const buildHtml = () => {
+            htmlContent += `
+                <div class="input-field col s10">
+                    <span class="prefix light-blue-text text-darken-4 ` +
+                        `center-align" data-question="${qId}"` +
+                        `data-multi="${multiCount}">`;
+            if (multiCount === 3) {
+                htmlContent += `
+                <a href="#!" onclick="removeMulti(this)">-</a>` +
+                `<a href="#!" onclick="addMulti(this)">+</a>`;
+            }
+            htmlContent += `
+                    </span>
+                    <input id="answer_${qId}-${multiCount}" ` +
+                        `name="answer_${qId}-${multiCount}" ` +
+                        `type="text" minlength="1" maxlength="100" ` +
+                        `class="validate" value="" required>
+                    <label for="answer_${qId}-${multiCount}" ` +
+                        `data-error="Invalid Answer" ` +
+                        `data-default="Answer ${qId}-${multiCount}">` +
+                        `Answer ${qId}-${multiCount}</label>
+                </div>
+                <div class="input-field col s2 checkbox-container ` +
+                    `inline-input center-align">
+                    <label class="center-align full-width">
+                        <input id="correct_${qId}-${multiCount}" ` +
+                            `name="correct_${qId}-${multiCount}" ` +
+                            `type="checkbox"/>
+                        <span></span>
+                    </label>
+                </div>`;
+            if (multiCount < 3) {
+                multiCount += 1;
+                buildHtml();
+            }
+        };
+
+        buildHtml();
+    }
+
+    targetTitle.html(htmlTitle);
+    targetField.html(htmlContent);
+}
+
+function checkBoxChanged(elem) {
+    let name = $(elem).attr('name');
+    switch (name) {
+    case 'quizMulti':
+        checkBoxMulti(elem);
+        break;
+    }
+}
+
+function listenToCheckbox() {
+    $('input[type="checkbox"]').
+        on("change", (e) => checkBoxChanged(e.currentTarget));
+}
+
 $(function() {
+    listenToCheckbox();
     listenToSelect();
     setSelectValue($('#quizCategory'), 'general knowledge');
     setSelectValue($('#roundCategory'), 'general knowledge');
