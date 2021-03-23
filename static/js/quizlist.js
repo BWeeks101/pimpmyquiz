02b3cd87c01e03f8299c2bd9f0888e9925111c39
Barry Weeks <bweeks101@gmail.com>
2021-03-23 13:30:12 +0000

    Added getQuizSearchResults() Returns quiz search results Added quizSearchCreateListeners() Creates listeners on search input and button elems

--- static/js/quizlist.js
+++ static/js/quizlist.js
@@ -1,1 +1,38 @@
+/* global addRecordPositions, inputHelperLabel, xHttpRequest */
+
+function getQuizSearchResults() {
+    let value = $('#quizSearch').val();
+    if (value === "" || value === undefined || value.length < 2) {
+        return;
+    }
+    let request = {
+        'type':
+            'quizSearch',
+        'params': {
+            'searchStr': value,
+            'page': 1
+        }
+    };
+    addRecordPositions({
+        'quizSearch': request.params.searchStr,
+        'currentPage': 1
+    });
+    xHttpRequest(request, $('#quizSearchResults')[0]);
+}
+
+function quizSearchCreateListeners() {
+    $("#quizSearch").on("focusout", () => inputHelperLabel("quizSearch"));
+
+    $("#quizSearch").on("keyup", (e) => {
+        if (e.key === "Enter") {
+            inputHelperLabel("quizSearch");
+            getQuizSearchResults();
+        }
+    });
+
+    $("#searchButton").
+        on("click", () => getQuizSearchResults());
+}
+
 $('#quizCollection .results-control .results-control-first')[0].click();
+quizSearchCreateListeners();
