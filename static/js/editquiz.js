d00872df908f6785be557b6cc2a55401e9cc058a
Barry Weeks <bweeks101@gmail.com>
2021-04-05 19:36:37 +0100

    Added listenToCollapsibleHeaders() Adds onClick listener to .collapsible-header elems Listener calls focusout on .img-url inputs in paired .collapsible-body (displays image previews)

---
+++ static/js/editquiz.js
@@ -0,0 +1,22 @@
+function listenToCollapsibleHeaders() {
+    $('.collapsible-header').on('click', (e) => {
+        let self = $(e.currentTarget);
+        let imgUrl;
+        let img;
+        if (self.parent().hasClass('active') === false) {
+            imgUrl = $(self).next().
+                find('.img-url');
+            img = $(imgUrl).parent().
+                next().
+                children('img');
+            if ((!$(img)[0]) ||
+                ($(img).height() === 0 && $(img).width() === 0)) {
+                $(imgUrl).trigger('focusout');
+            }
+        }
+    });
+}
+
+$(function () {
+    listenToCollapsibleHeaders();
+});
