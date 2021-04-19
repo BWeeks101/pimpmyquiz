4311b855177f0bb5b75e7d8f5c585cc529a6252e
Barry Weeks <bweeks101@gmail.com>
2021-04-19 14:36:27 +0100

    Altered doc ready func Removed MaterializeCSS Tooltip init call

--- static/js/editusers.js
+++ static/js/editusers.js
@@ -275,7 +275,6 @@ function userSearchCreateListeners() {
 $(function() {
     $('.collapsible').collapsible();
     $('select').formSelect();
-    $('.tooltipped').tooltip();
     $('.modal').modal();
     listenToUserRoleCollapsibleHeaders();
     listenToUserSearchCollapsibleHeaders();
