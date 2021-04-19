b6db10c1b27ab26cbb00e4c0c71aa654e30f9ad2
Barry Weeks <bweeks101@gmail.com>
2021-04-19 14:53:32 +0100

    Altered loadImg() Renamed pyList to roundList Added check to handle null/undefined img urls Removed unnecessary if statements Renamed resetQuizTitleInput() to resetModalQuizTitleInput() Altered doc ready func Updated initDupTitleModal() callback param Removed MaterializeCSS Tooltip init call

--- static/js/viewquiz.js
+++ static/js/viewquiz.js
@@ -20,28 +20,41 @@ function listenToQuizImg(elem) {
 }
 
 // eslint-disable-next-line no-unused-vars
-function loadImg(pyList) {
-    pyList.forEach(function (round) {
+function loadImg(roundList) {
+    const isValidData = (url) => {
+        switch (url) {
+        case null:
+        case undefined:
+            url = '';
+            break;
+        default:
+        }
+
+        return url;
+    };
+
+    roundList.forEach(function (round) {
         let roundNum = round.round_num;
         round.questions.forEach(function (question) {
             let questionNum = question.question_num;
-            if (question.question_img_url.length > 0) {
-                $(`#img_q_${roundNum}_${questionNum}`).
-                    attr('src', question.question_img_url);
-            }
+            // eslint-disable-next-line camelcase
+            question.question_img_url = isValidData(question.question_img_url);
+            $(`#img_q_${roundNum}_${questionNum}`).
+                attr('src', question.question_img_url);
+
             if (question.multiple_choice === false) {
-                if (question.answer_img_url &&
-                        question.answer_img_url.length > 0) {
-                    $(`#img_a_${roundNum}_${questionNum}`).
-                        attr('src', question.answer_img_url);
-                }
+                // eslint-disable-next-line camelcase
+                question.answer_img_url = isValidData(question.answer_img_url);
+                $(`#img_a_${roundNum}_${questionNum}`).
+                    attr('src', question.answer_img_url);
+
             } else {
                 question.multiple_choice_options.forEach(function (option) {
                     let optionNum = option.option_num;
-                    if (option.answer_img_url.length > 0) {
-                        $(`#img_o_${roundNum}_${questionNum}_${optionNum}`).
-                            attr('src', option.answer_img_url);
-                    }
+                    // eslint-disable-next-line camelcase
+                    option.answer_img_url = isValidData(option.answer_img_url);
+                    $(`#img_o_${roundNum}_${questionNum}_${optionNum}`).
+                        attr('src', option.answer_img_url);
                 });
             }
         });
@@ -54,7 +67,7 @@ function listenToCopyQuiz() {
     });
 }
 
-function resetQuizTitleInput() {
+function resetModalQuizTitleInput() {
     $('#modalQuizTitle').attr('data-prev', '');
     $('#modalQuizTitle:hidden').
     val($('#quizOriginalTitle').html());
@@ -62,9 +75,8 @@ function resetQuizTitleInput() {
 
 $(function() {
     if ($('#dupTitleModal').length) {
-        initDupTitleModal(resetQuizTitleInput);
+        initDupTitleModal(resetModalQuizTitleInput);
         listenToCopyQuiz();
-        $('.tooltipped').tooltip();
     }
     $('.quiz-image').each((i, elem) => listenToQuizImg(elem));
 });
