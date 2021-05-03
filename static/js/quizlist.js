5b146d8436e9ff4542e342160efbc4046cc0fa5a
Barry Weeks <bweeks101@gmail.com>
2021-05-03 22:31:39 +0100

    Added comments throughout Replaced refs to #modalSubmitButton with #modalCopyQuizButton Altered getQuizSearchResults() Set default false value for isGlobal property isGlobal existence check altered to isGlobal === true

--- static/js/quizlist.js
+++ static/js/quizlist.js
@@ -3,17 +3,25 @@
 initDupTitleModal, popChangeConfModal, initChangeConfModal,
 listenToChangeConfModalButtons, observeResults, closeToolTip, getInputHelper */
 
+/* Send a server call to delete a quiz */
+/* Requires a quizId passed as a string or integer */
 // eslint-disable-next-line no-unused-vars
 function deleteQuiz(quizId) {
     open(`/delete_quiz?&id=${quizId}`, '_self');
 }
 
+/* Add click listeners to a elems within .results-data containers */
 function listenToResultsATags() {
+
+    /* Close the MaterializeCSS tooltip when the link is clicked */
+    /* Prevents inconsistent tooltip display behaviour when browser */
+    /* navigation buttons are used on mobile devices after a link is clicked */
     $('.results-data a').on('click', (e) => {
         closeToolTip(e.currentTarget);
     });
 }
 
+/* Add click listeners to copy quiz links */
 function listenToCopyQuizLinks() {
     $('.copy-quiz').on("click", (e) => {
         let self = e.currentTarget;
@@ -21,13 +29,14 @@ function listenToCopyQuizLinks() {
             find('.quiz-title').
             html());
 
-        $('#modalSubmitButton').
+        $('#modalCopyQuizButton').
             attr('data-quizId', $(self).attr('data-quizId'));
 
         copyQuizListener();
     });
 }
 
+/* Add click listeners to delete quiz links */
 function listenToDelLinks() {
     $('.del-quiz').on('click', (e) => {
         e.stopPropagation();
@@ -35,7 +44,10 @@ function listenToDelLinks() {
     });
 }
 
-function getQuizSearchResults(isGlobal) {
+/* Format search request and execute xHttp call to search */
+/* Optional isGlobal parameter defines whether the search is a global quiz */
+/* search, or a current user quiz search (default) */
+function getQuizSearchResults(isGlobal = false) {
     let value = $('#quizSearch').val();
     let category = $('#quizCategory').val();
     if (value === "" || value === undefined || value.length < 1) {
@@ -50,7 +62,7 @@ function getQuizSearchResults(isGlobal) {
             category
         }
     };
-    if (isGlobal) {
+    if (isGlobal === true) {
         request.type = 'globalQuizSearch';
     }
     addRecordPositions({
@@ -60,15 +72,19 @@ function getQuizSearchResults(isGlobal) {
     xHttpRequest(request, $('#quizSearchResults')[0]);
 }
 
+/* Adds keyup listener to quizSearch input, and click listener to */
+/* searchButton */
 function quizSearchCreateListeners() {
+
+    /* Execute Search on Enter keypress */
     $("#quizSearch").on("keyup", (e) => {
         let isGlobal;
         if (e.key === "Enter") {
             isGlobal = $(e.currentTarget).parent().
                 parent().
-                nextAll('.search-action-container').
-                children('button[data-type="global"]').
-                attr('data-type');
+                    nextAll('.search-action-container').
+                        children('button[data-type="global"]').
+                            attr('data-type');
             if (isGlobal) {
                 getQuizSearchResults(true);
             } else {
@@ -77,6 +93,7 @@ function quizSearchCreateListeners() {
         }
     });
 
+    /* Execute Search on button click */
     $("#searchButton").on("click", (e) => {
         let isGlobal = $(e.currentTarget).attr('data-type');
         if (isGlobal) {
@@ -87,6 +104,8 @@ function quizSearchCreateListeners() {
     });
 }
 
+/* Adds click listener to .subopt elems within select box */
+/* Allows custom styled select options to function */
 function listenToSelect() {
     let selector = ".select-container .select-wrapper ";
     selector += "ul li.optgroup span div.subopt";
@@ -99,6 +118,7 @@ function listenToSelect() {
     });
 }
 
+/* Returns the first page of quiz results */
 function getInitialQuizList() {
     let request = {
         'type':
@@ -117,6 +137,7 @@ function getInitialQuizList() {
     xHttpRequest(request, $('#quizSearchResults')[0]);
 }
 
+/* Initialise listeners, tooltips for elems returned within search results  */
 function initEmbeddedSearchResultControls() {
     listenToResultsATags();
     listenToCopyQuizLinks();
@@ -124,14 +145,17 @@ function initEmbeddedSearchResultControls() {
     $('.results-data .tooltipped').tooltip();
 }
 
+/* Reset dupTitleModal elem values/data */
 function resetModalQuizTitleInput() {
     $('#modalQuizTitle').attr('data-prev', '');
     $('#modalQuizTitle:hidden').val('');
     getInputHelper($('#modalQuizTitle')).close();
-    $('#modalSubmitButton').attr('data-quizId', '');
+    $('#modalCopyQuizButton').attr('data-quizId', '');
     listenToCopyQuizLinks();
 }
 
+/* When document is ready, get initial quiz list, initialise listeners, */
+/* modals, search result observer and returned controls */
 $(function() {
     $('select').formSelect();
     getInitialQuizList();
