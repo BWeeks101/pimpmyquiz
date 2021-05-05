fb48455deee6b821b55be64ca89ce949cc16c098
Barry Weeks <bweeks101@gmail.com>
2021-05-05 19:41:24 +0100

    Added comments throughout

--- static/js/register.js
+++ static/js/register.js
@@ -1,14 +1,19 @@
 /* global pWordValidation */
 
+/* Add submit listener to #register form */
 function listenToRegisterFormSubmission() {
     $('#register').submit((e) => {
-        e.preventDefault();
+        e.preventDefault(); // Prevent the default link click action
+
+        // If password validation returns true, then submit the form
         if (pWordValidation('pwd', 'cPwd') === true) {
             $('#register')[0].submit();
         }
     });
 }
 
+/* Document Ready Function */
 $(function () {
+    // Initialise form submit listener
     listenToRegisterFormSubmission();
 });
