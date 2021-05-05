78d42db998bf299d77a99ea19310d6e983bea05f
Barry Weeks <bweeks101@gmail.com>
2021-05-05 19:50:48 +0100

    Altered eslint comment config Added comments throughout Removed listenToSelect() Altered listenToMultiControls() Added e.stopPropagation() call to listeners Altered listenToQuizInputs() Removed unnecessary false parameter from inputValidation() call Altered stopListeningToQuizInputHelpers() Made .find() selector more specific Altered listenToCancelUrl() Added type variable instead of multiple popChangeConfModal() calls Altered listenToCheckbox() Added type variable instead of multiple popChangeConfModal() calls

--- static/js/newquizlisteners.js
+++ static/js/newquizlisteners.js
@@ -1,8 +1,9 @@
 /*eslint func-style: ["error", "declaration", { "allowArrowFunctions": true }]*/
-/* global setSelectValue, addRound, addQ, removeMulti, addMulti, removeRound,
+/* global addRound, addQ, removeMulti, addMulti, removeRound,
 removeQ, imgPreviewError, imgPreviewLoad, popChangeConfModal, closeToolTip,
 inputValidation, quizFormValidation, M, validateCorrectCheckboxes */
 
+/* Remove click listener from .subopt elements */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToSelect() {
     let selector = ".select-container .select-wrapper ";
@@ -10,123 +11,114 @@ function stopListeningToSelect() {
     $(selector).off("click");
 }
 
-// eslint-disable-next-line no-unused-vars
-function listenToSelect() {
-    let selector = ".select-container .select-wrapper ";
-    selector += "ul li.optgroup span div.subopt";
-    $(selector).on("click", (e) => {
-        let self = e.currentTarget;
-        let selectContainer = self.closest('.select-container');
-        let select = $('.select-wrapper select', selectContainer);
-        let value = self.innerText.trim().toLowerCase();
-        setSelectValue(select, value);
-
-        if (select.attr('id') === 'quizCategory') {
-            setSelectValue($('.round-category'), value);
-
-            //Use Attr for disabled instead of Prop.
-            //MaterializeCSS seems to rely on the presence of the Attr.
-            if (value === 'general knowledge') {
-                $('.round-category').each((i, el) => {
-                    $(el).removeAttr('disabled');
-                });
-
-            } else {
-                $('.round-category').each((i, el) => {
-                    $(el).attr('disabled', true);
-                });
-            }
-        }
-
-    });
-}
-
+/* Removed click listeners from .rcontrols- elements */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToRControls() {
     $('.rcontrols-remove').off("click");
     $('.rcontrols-add').off("click");
 }
 
+/* Add click listeners to .rcontrols- elements */
 // eslint-disable-next-line no-unused-vars
 function listenToRControls() {
+    // Define the .rcontrols-remove click listener
     $('.rcontrols-remove').on("click", (e) => {
-        e.stopPropagation();
-        closeToolTip(e.currentTarget);
-        removeRound(e.currentTarget);
+        e.stopPropagation(); // Prevent the default link click action
+        closeToolTip(e.currentTarget); // Close the tooltip for this element
+        removeRound(e.currentTarget); // call removeRound()
     });
 
+    // Define the .rcontrols-add click listener
     $('.rcontrols-add').on("click", (e) => {
-        e.stopPropagation();
-        closeToolTip(e.currentTarget);
-        addRound(e.currentTarget);
+        e.stopPropagation(); // Prevent the default link click action
+        closeToolTip(e.currentTarget); // Close the tooltip for this element
+        addRound(e.currentTarget); // call addRound()
     });
 }
 
+/* Remove click listeners from .qcontrols- elements */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToQControls() {
     $('.qcontrols-remove').off("click");
     $('.qcontrols-add').off("click");
 }
 
+/* Add click listeners to .qcontrols- elements */
 // eslint-disable-next-line no-unused-vars
 function listenToQControls() {
+    // Define the .qcontrols-remove click listener
     $('.qcontrols-remove').on("click", (e) => {
-        e.stopPropagation();
-        closeToolTip(e.currentTarget);
-        removeQ(e.currentTarget);
+        e.stopPropagation(); // Prevent the default link click action
+        closeToolTip(e.currentTarget); // Close the tooltip for this element
+        removeQ(e.currentTarget); // Call removeQ()
     });
 
+    // Define the .qcontrols-add click listener
     $('.qcontrols-add').on("click", (e) => {
-        e.stopPropagation();
-        closeToolTip(e.currentTarget);
-        addQ(e.currentTarget);
+        e.stopPropagation(); // Prevent the default link click action
+        closeToolTip(e.currentTarget); // Close the tooltip for this element
+        addQ(e.currentTarget); // Call addQ()
     });
 }
 
+/* Remove click listeners from .multi-control- elements */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToMultiControls() {
-    $('a.multi-control-remove').
-        off("click");
-    $('a.multi-control-add').
-        off("click");
+    $('a.multi-control-remove').off("click");
+    $('a.multi-control-add').off("click");
 }
 
+/* Add click listeners to .multi-control- elements */
 // eslint-disable-next-line no-unused-vars
 function listenToMultiControls() {
-    $('a.multi-control-remove').
-        on("click", (e) => {
-            closeToolTip(e.currentTarget);
-            removeMulti(e.currentTarget);
-        });
-    $('a.multi-control-add').
-        on("click", (e) => {
-            closeToolTip(e.currentTarget);
-            addMulti(e.currentTarget);
-        });
+    // define the .multi-control-remove click listener
+    $('a.multi-control-remove').on("click", (e) => {
+        e.stopPropagation(); // Prevent the default link click action
+        closeToolTip(e.currentTarget); // Close the tooltip for this element
+        removeMulti(e.currentTarget); // Call removeMulti()
+    });
+
+    // define the .multi-control-add click listener
+    $('a.multi-control-add').on("click", (e) => {
+        e.stopPropagation(); // Prevent the default link click action
+        closeToolTip(e.currentTarget); // Close the tooltip for this element
+        addMulti(e.currentTarget); // Call addMulti()
+    });
 }
 
+/* Remove input listeners from inputs in the specified container */
+/* Requires: */
+/*  container: div element containing the inputs */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToQuizInputs(container) {
+    // Ignore checkbox inputs
     $(container).
-        find('input:not(.img-url, [type="checkbox"])').
+        find('input:not([type="checkbox"])').
             off('input');
 }
 
+/* Add input listeners to inputs in the specified container */
+/* Requires: */
+/*  container: div element containing the inputs */
 // eslint-disable-next-line no-unused-vars
 function listenToQuizInputs(container) {
-    // $(container).
-    //     find('input:not(.img-url, [type="checkbox"])').
-    //         on('input', (e) => inputValidation(e.currentTarget, false));
-
+    // Ignore checkbox inputs
+    // On input, call inputValidation()
     $(container).
         find('input:not([type="checkbox"])').
-            on('input', (e) => inputValidation(e.currentTarget, false));
+            on('input', (e) => inputValidation(e.currentTarget));
 }
 
+/* Remove MaterializeCSS Collapsible instances from helpers in the specified */
+/* container */
+/* Requires: */
+/*  container: div element containing the helper collapsibles */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToQuizInputHelpers(container) {
-    $(container).find('.collapsible').
-        each((i, elem) => {
+    // (i declaration required by jQuery .each)
+    $(container).find('.collapsible.helper-collapsible').
+        each((i, elem) => { // For each .helper-collapsible...
+            // Get the MaterializeCSS instance for the element, and destroy it
             let instance = M.Collapsible.getInstance(elem);
             if (instance) {
                 instance.destroy();
@@ -134,83 +126,69 @@ function stopListeningToQuizInputHelpers(container) {
         });
 }
 
+/* Initialise MaterializeCSS Collapsible instances for helpers in the */
+/* specified container */
+/* Requires: */
+/*  container: div element containing the helper collapsibles */
 // eslint-disable-next-line no-unused-vars
 function listenToQuizInputHelpers(container) {
+    // Initialise MaterializeCSS Collapsible instances for .helper-collapsibles
     $(container).find('.collapsible.helper-collapsible').
         collapsible();
 }
 
-// eslint-disable-next-line no-unused-vars
-// function stopListeningToImgInputs() {
-//     // $('input.img-url').off("focusout");
-//     // $('input.img-url').off("keyup");
-//     $('input.img-url').off("input");
-// }
-
-// eslint-disable-next-line no-unused-vars
-// function listenToImgInputs() {
-//     $('input.img-url').
-//         on("input", (e) => inputValidation(e.currentTarget, false));
-
-//     // const updImgPreview = (elem) => {
-//     //     checkImgUrl(elem);
-//     //     imgPreview($(elem).val(), $(elem).
-//     //         closest('.input-field').
-//     //         next());
-//     // };
-
-//     // $('input.img-url').
-//     //     on("focusout", (e) => {
-//     //         updImgPreview(e.currentTarget);
-//     //     });
-
-//     // $('input.img-url').
-//     //     on("keyup", (e) => {
-//     //         if (e.key === 'Enter' ||
-//     //                 e.key === 'Delete' ||
-//     //                 e.key === 'Backspace') {
-//     //             // updImgPreview(e.currentTarget);
-//     //             inputValidation(e.currentTarget, true);
-//     //         }
-//     //     });
-// }
-
+/* Remove error/load listeners from specified element */
+/* Requires: */
+/*  elem: .image-preview>img element */
 function stopListeningToImgPreview(elem) {
     $(elem).off("error");
     $(elem).off("load");
 }
 
+/* Add error/load listeners to specified element */
+/* Requires: */
+/*  elem: .image-preview>img element */
 // eslint-disable-next-line no-unused-vars
 function listenToImgPreview(elem) {
-    stopListeningToImgPreview(elem);
+    stopListeningToImgPreview(elem); // Remove any existing error/load listeners
+
+    // Add error listener.  On error call imgPreviewError()
     $(elem).on("error", () => imgPreviewError(elem,
         `Unable to preview Image.<br>Please check the URL.`));
+
+    // Add load listener.  On load call imgPreviewLoad()
     $(elem).on("load", () => imgPreviewLoad(elem, $(elem).
             closest('.collapsible-body').
-            width())
+                width())
     );
 }
 
+/* Add click listener to #submitButton element */
 // eslint-disable-next-line no-unused-vars
 function listenToSubmitButton() {
     $('#submitButton').on("click", (e) => {
-        e.preventDefault();
-        quizFormValidation();
+        e.preventDefault(); // Prevent the default submit button click action
+        quizFormValidation(); // Call quizFormValidation()
     });
 }
 
+/* Add click listener to #cancelUrl element */
 // eslint-disable-next-line no-unused-vars
 function listenToCancelUrl() {
     $('#cancelUrl').on('click', (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Prevent the default link click action
+        let type = 'cn'; // set type to 'cn' (cancel new)
         if ($('#editQuiz').length) {
-            popChangeConfModal('ce', e.currentTarget);
-            return;
+            // If this is a quiz edit, set type to 'ce' (cancel edit)
+            type = 'ce';
         }
-        popChangeConfModal('cn', e.currentTarget);
+        // Populate and call the change confirmation modal
+        popChangeConfModal(type, e.currentTarget);
     });
 }
 
+/* Remove change listener from .correct Checkbox elements within the */
+/* specified container */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToCorrectCheckboxes(container) {
     $(container).
@@ -218,27 +196,33 @@ function stopListeningToCorrectCheckboxes(container) {
             off('change');
 }
 
+/* Add change listener to .correct Checkbox elements within the specified */
+/* container */
 // eslint-disable-next-line no-unused-vars
 function listenToCorrectCheckboxes(container) {
+    // On change, call validateCorrectCheckboxes()
     $(container).
         find('.correct[type="checkbox"]').
             on('change', (e) => validateCorrectCheckboxes(e.currentTarget));
 }
 
+/* Remove change listener from .quizMulti Checkbox elements */
 // eslint-disable-next-line no-unused-vars
 function stopListeningToCheckbox() {
     $('input[type="checkbox"].quizMulti').
         off("change");
 }
 
+/* Add change listener to .quizMulti Checkbox elements */
 // eslint-disable-next-line no-unused-vars
 function listenToCheckbox() {
     $('input[type="checkbox"].quizMulti').on('change', (e) => {
-        let self = e.currentTarget;
-        if (!$(self).prop('checked')) {
-            popChangeConfModal('mu', self);
-            return;
+        let type = 'mc'; // set type to 'mc' (multiple choice)
+        if (!$(e.currentTarget).prop('checked')) {
+            // If the .quizMulti is checked, set type to 'sa' (single answer)
+            type = 'sa';
         }
-        popChangeConfModal('mc', self);
+        // Populate and call the change confirmation modal
+        popChangeConfModal(type, e.currentTarget);
     });
 }
