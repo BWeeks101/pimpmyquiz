c4ef67793495db9764805df5056ba56d9d3b9acb
Barry Weeks <bweeks101@gmail.com>
2021-05-07 16:41:26 +0100

    Altered eslint comment config Altered listenToRegisterFormSubmission() Altered submit event listener Added invalid() (called by userIdValidate() on validation failure) Added valid() (called by userIdValidate() on validation success) Moved pWordValidation() logic inside valid() Added userIdValidate() call Altered doc ready func Added listenToUserId() call

--- static/js/register.js
+++ static/js/register.js
@@ -1,19 +1,31 @@
-/* global pWordValidation */
+/*eslint func-style: ["error", "declaration", { "allowArrowFunctions": true }]*/
+/* global pWordValidation, userIdValidate, listenToUserId */
 
 /* Add submit listener to #register form */
 function listenToRegisterFormSubmission() {
     $('#register').submit((e) => {
         e.preventDefault(); // Prevent the default link click action
 
-        // If password validation returns true, then submit the form
-        if (pWordValidation('pwd', 'cPwd') === true) {
-            $('#register')[0].submit();
-        }
+        // callback functions for userIdValidate() call
+        const invalid = () => false;
+        const valid = () => {
+            // If password validation returns true, then submit the form
+            if (pWordValidation('pwd', 'cPwd') === true) {
+                $('#register')[0].submit();
+            }
+        };
+
+        /* Call userIdValidate() */
+        /* On invalid, return.  On valid, submit the registration form */
+        userIdValidate(invalid, valid);
     });
 }
 
 /* Document Ready Function */
 $(function () {
+    // Initialise input listener for user_id input
+    listenToUserId();
+
     // Initialise form submit listener
     listenToRegisterFormSubmission();
 });
